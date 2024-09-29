956693cc4fe7bd46f56b173bb61db82c32691d0a
nguyenluc25 <nguyenluc1234012@gmail.com>
2024-09-29 21:20:22 +0700

    first commit

---
+++ src/main/java/vn/edu/iuh/week02_lab_nguyentheluc_21046661/MyApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.week02_lab_nguyentheluc_21046661;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MyApplication extends Application {
+
+}
